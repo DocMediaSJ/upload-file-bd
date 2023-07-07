c8b71451f3d52555e481b1028239ec215ec94202
matiasetudela <matiasetudela@gmail.com>
2023-07-07 17:34:19 -0300

    hmtl basico y carga archivo en en una carpeta

---
+++ upload-files-index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cargar archivo</title>
+</head>
+<body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Cargar archivo" name="submit">
+    </form>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,39 @@
+<?php
+$targetDirectory = "archivos/"; // Reemplaza con la ruta a la carpeta deseada
+
+if(isset($_POST["submit"])) {
+    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Verificar si el archivo ya existe
+    if (file_exists($targetFile)) {
+        echo "El archivo ya existe.";
+        $uploadOk = 0;
+    }
+
+    // Verificar el tamaño máximo del archivo (opcional)
+    if ($_FILES["fileToUpload"]["size"] > 5000000) {
+        echo "El archivo es demasiado grande.";
+        echo "<br>";
+        $uploadOk = 0;
+    }
+
+    // Permitir solo ciertos formatos de archivo (opcional)
+    if($imageFileType != "pdf" && $imageFileType != "doc" && $imageFileType != "docx"
+        && $imageFileType != "odt" ) {
+        echo "Solo se permiten archivos .pdf, .doc, docx. y .odt.";
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+        echo "Error al cargar el archivo.";
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+            echo "El archivo ". basename( $_FILES["fileToUpload"]["name"]). " se ha cargado correctamente.";
+        } else {
+            echo "Error al cargar el archivo.";
+        }
+    }
+}
+?>
