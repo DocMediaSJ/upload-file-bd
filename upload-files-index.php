bf60fc1846db1a9cf11829adf13a70b959189b18
matiasetudela <matiasetudela@gmail.com>
2023-07-07 17:54:17 -0300

    hmtl basico con guardado en la base de datos

--- upload-files-index.php
+++ upload-files-index.php
@@ -2,12 +2,23 @@
 <html>
 <head>
     <title>Cargar archivo</title>
+    <script>
+        function validateForm() {
+            var fileInput = document.getElementById("fileToUpload");
+            if (fileInput.files.length === 0) {
+                alert("Por favor, selecciona un archivo.");
+                return false;
+            }
+            return true;
+        }
+</script>
 </head>
 <body>
-    <form action="upload.php" method="POST" enctype="multipart/form-data">
+    <form action="upload.php" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
         <input type="file" name="fileToUpload" id="fileToUpload" accept=".pdf,.doc,.docx">
         <br>
         <input type="submit" value="Cargar archivo" name="submit">
     </form>
 </body>
 </html>
+
--- upload.php
+++ upload.php
@@ -1,7 +1,18 @@
 <?php
 $targetDirectory = "archivos/"; // Reemplaza con la ruta a la carpeta deseada
+$databaseHost = "localhost"; // Cambia según tu configuración de base de datos
+$databaseName = "test"; // Cambia según tu configuración de base de datos
+$databaseUser = "root"; // Cambia según tu configuración de base de datos
+$databasePassword = ""; // Cambia según tu configuración de base de datos
 
-if(isset($_POST["submit"])) {
+// Crear una conexión a la base de datos
+$conn = new mysqli($databaseHost, $databaseUser, $databasePassword, $databaseName);
+
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+if (isset($_POST["submit"])) {
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
@@ -21,9 +32,8 @@ if(isset($_POST["submit"])) {
     }
 
     // Permitir solo ciertos formatos de archivo (opcional)
-    if($imageFileType != "pdf" && $imageFileType != "doc" && $imageFileType != "docx"
-        && $imageFileType != "odt" ) {
-        echo "Solo se permiten archivos .pdf, .doc, docx. y .odt.";
+    if ($imageFileType != "pdf" && $imageFileType != "doc" && $imageFileType != "docx") {
+        echo "Solo se permiten archivos PDF, DOC y DOCX.";
         echo "<br>";
         $uploadOk = 0;
     }
@@ -33,11 +43,39 @@ if(isset($_POST["submit"])) {
         echo "<br>";
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "El archivo ". basename( $_FILES["fileToUpload"]["name"]). " se ha cargado correctamente.";
+            // Insertar información del archivo en la base de datos
+            $nombreArchivo = $_FILES["fileToUpload"]["name"];
+            $rutaArchivo = $targetFile;
+            $sql = "INSERT INTO bdarchivos (nombre, ruta) VALUES ('$nombreArchivo', '$rutaArchivo')";
+            if ($conn->query($sql) === TRUE) {
+                echo "El archivo " . basename($_FILES["fileToUpload"]["name"]) . " se ha cargado correctamente.";
+                echo "<br>";
+            } else {
+                echo "Error al cargar el archivo en la base de datos: " . $conn->error;
+                echo "<br>";
+            }
         } else {
             echo "Error al cargar el archivo.";
             echo "<br>";
         }
     }
 }
+
+// Obtener los archivos almacenados en la base de datos
+$sql = "SELECT * FROM bdarchivos";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<h2>Archivos cargados:</h2>";
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li><a href='" . $row["ruta"] . "' target='_blank'>" . $row["nombre"] . "</a></li>";
+    }
+    echo "</ul>";
+} else {
+    echo "<p>No se han cargado archivos.</p>";
+    echo "<br>";
+}
+
+$conn->close();
 ?>
